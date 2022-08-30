f749974130987af3a37171b3be29cf768f9a295a
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2022-08-30 09:20:25 +0530

    Aug-30 Code Types of Variables
    Aug-30 Code Types of Variables , Garbage Collector etc

--- Car.java
+++ Car.java
@@ -7,14 +7,31 @@ public class Car {
 	int seatingCapacity;
 	String fuelType;
 	boolean isAutomatic;
+	static int noOfWheels = 4;
 	
 	
 	public void drive()
 	{
+		int x = 20;	
+		//System.out.println(noOfWheels);
+		// Non static methods can access both static and non static
+		// variables and methods..
 		System.out.println(" The car of the colour "+colour +" no of gears are "+noOfGears +
 				"which has the seating capacity of "+seatingCapacity +" Fuel type "+fuelType + 
 				" Which is automatic "+isAutomatic +" is been driven ");
 	}
+	
+	public static void test()
+	{
+		System.out.println("This is a test method...");
+		//System.out.println(colour);
+		// Static methods can only acces static global vriables
+		// and they can call other static methods.
+		// test1();
+	}
+	
+	
+	
 	
 
 }
--- TestCar.java
+++ TestCar.java
@@ -1,5 +1,7 @@
 package com.corejava.practice;
 
+import java.util.Arrays;
+
 public class TestCar {
 
 	public static void main(String[] args) {
@@ -13,11 +15,15 @@ public class TestCar {
 		
 		Car c = new Car();
 		
+		System.out.println(c.noOfGears);
+		
 		c.noOfGears = 5;
 		c.colour = "White";
 		c.seatingCapacity = 5;
 		c.fuelType = "Petrol";
 		c.isAutomatic = true;
+		System.out.println(c.noOfWheels);
+		System.out.println(Car.noOfWheels);
 		
 		c.drive();
 		
@@ -29,8 +35,47 @@ public class TestCar {
 		c1.isAutomatic = false;
 		
 		c1.drive();
+		System.out.println(c1.noOfWheels);
+		System.out.println(Car.noOfWheels);
+		
+		c=c1;
+		// The garbage collector is part of JVM , which will delete all the objects which
+		// are not referred by any reference variables
+		// In the above example the object which was pointed by c will be garbage collected....
+		
+		System.out.println("++++++++++++++++++++++++++");
 		
 		
+		System.out.println(c.colour);
+		System.out.println(c1.colour);
+		
+		c= null;
+		
+		System.out.println(c.colour);
+		
+		Car.test();
+		// Arrays.sort(null);  Arrays is a class and sort is  a static method.
+		
+		
+		/*
+		
+		//Car[] cArray = new Car[2];
+		 * 
+		 * String[] s = {"java","Selenium","Automation"};
+		
+		// We can create the Arrays of Car type to store Car Objects
+		
+		System.out.println("====================");
+		
+		Car[] cArray = {c,c1};
+		
+		for(Car m : cArray)
+		{
+			System.out.println(m.colour);
+			m.drive();
+		}
+		
+		*/
 		
 		
 		
