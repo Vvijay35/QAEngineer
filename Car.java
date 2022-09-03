e60d3ca6882fca2f0fe2963fcc36455544db6469
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2022-09-03 14:20:57 +0530

    Sept3-Code -OOPS
    OOPS- Data Hiding and Encapsulation

--- Car.java
+++ Car.java
@@ -1,37 +1,79 @@
-package com.corejava.practice;
+package com.corejava.oops;
 
 public class Car {
 	
-	int noOfGears;
-	String colour;
-	int seatingCapacity;
-	String fuelType;
-	boolean isAutomatic;
-	static int noOfWheels = 4;
+	// Data Hiding = Hide your data members from out side code by declaring them as private
+	// Encapsulation = Write public getters and setters  and bind the data members with corresponding setters/getters
+	
+	private String colour;
+	private int seatingCapacity;
+	private int engineCapacity;
+	
+	public void setColour(String colour)
+	{
+		String[] colours = {"Black","White","Yellow","Grey"};
+		
+		if( ( colour.equalsIgnoreCase("black")) || (colour.equalsIgnoreCase("white")))
+			{
+					this.colour = colour;
+			}
+		else
+		{
+			System.out.println(" The colour cant be set to the car...");
+		}
+	}
+	
+	public String getColour()
+	{
+		return colour;
+	}
 	
 	
+	
+	
+	public int getSeatingCapacity() {
+		return seatingCapacity;
+	}
+
+	public void setSeatingCapacity(int seatingCapacity) {
+		if((seatingCapacity == 5) || (seatingCapacity == 8 ))
+		{
+			this.seatingCapacity = seatingCapacity;
+		}
+		else
+		{
+			System.out.println(" Please enter valid seating capacity....");
+		}
+	}
+
+	public int getEngineCapacity() {
+		return engineCapacity;
+	}
+
+	public void setEngineCapacity(int engineCapacity) {
+		
+		if( (engineCapacity > 800) && (engineCapacity < 1500))
+		{
+			this.engineCapacity = engineCapacity;
+		}
+		else
+		{
+			System.out.println(" Please enter the valid seating capcity..");
+		}
+	}
+
+	public void dropYourFriendsToMovie()
+	{
+		System.out.println(" I have a "+ colour + " car I can drop only "+seatingCapacity +" Friends" );
+		
+	}
+
 	public void drive()
 	{
-		int x = 20;	
-		//System.out.println(noOfWheels);
-		// Non static methods can access both static and non static
-		// variables and methods..
-		System.out.println(" The car of the colour "+colour +" no of gears are "+noOfGears +
-				"which has the seating capacity of "+seatingCapacity +" Fuel type "+fuelType + 
-				" Which is automatic "+isAutomatic +" is been driven ");
+		if( engineCapacity > 800)
+		{
+			System.out.println(" The car of the colour "+ colour +" can be driven up to 150 KPMH");
+		}
 	}
-	
-	public static void test()
-	{
-		System.out.println("This is a test method...");
-		//System.out.println(colour);
-		// Static methods can only acces static global vriables
-		// and they can call other static methods.
-		// test1();
-	}
-	
-	
-	
-	
 
 }
--- TestCar.java
+++ TestCar.java
@@ -1,84 +1,23 @@
-package com.corejava.practice;
-
-import java.util.Arrays;
+package com.corejava.oops;
 
 public class TestCar {
 
 	public static void main(String[] args) {
-		
-		
-		// A Class is a blue print or Templete 
-		// A class is a userdefined Data Type
-		
-		//An Oject is an instance of the class , this is real entity
-		
-		
 		Car c = new Car();
 		
-		System.out.println(c.noOfGears);
+		//c.colour = "Black";
+		c.setColour("Black");
 		
-		c.noOfGears = 5;
-		c.colour = "White";
-		c.seatingCapacity = 5;
-		c.fuelType = "Petrol";
-		c.isAutomatic = true;
-		System.out.println(c.noOfWheels);
-		System.out.println(Car.noOfWheels);
+		//c.engineCapacity = 100;
+		c.setEngineCapacity(1000);
+		
+		//c.seatingCapacity = -5;
+		c.setSeatingCapacity(5);
 		
 		c.drive();
+		c.dropYourFriendsToMovie();
 		
-		Car c1 = new Car();
-		c1.noOfGears = 7;
-		c1.colour = "Black";
-		c1.seatingCapacity = 8;
-		c1.fuelType = "Diesel";
-		c1.isAutomatic = false;
-		
-		c1.drive();
-		System.out.println(c1.noOfWheels);
-		System.out.println(Car.noOfWheels);
-		
-		c=c1;
-		// The garbage collector is part of JVM , which will delete all the objects which
-		// are not referred by any reference variables
-		// In the above example the object which was pointed by c will be garbage collected....
-		
-		System.out.println("++++++++++++++++++++++++++");
-		
-		
-		System.out.println(c.colour);
-		System.out.println(c1.colour);
-		
-		c= null;
-		
-		System.out.println(c.colour);
-		
-		Car.test();
-		// Arrays.sort(null);  Arrays is a class and sort is  a static method.
-		
-		
-		/*
-		
-		//Car[] cArray = new Car[2];
-		 * 
-		 * String[] s = {"java","Selenium","Automation"};
-		
-		// We can create the Arrays of Car type to store Car Objects
-		
-		System.out.println("====================");
-		
-		Car[] cArray = {c,c1};
-		
-		for(Car m : cArray)
-		{
-			System.out.println(m.colour);
-			m.drive();
-		}
-		
-		*/
-		
-		
-		
+		System.out.println(" The engine capacity of the car is "+c.getEngineCapacity());
 
 	}
 
