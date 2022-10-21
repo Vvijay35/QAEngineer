4eed963349966db4f43c05356c1969edd0fdc20f
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2022-10-21 09:22:59 +0530

    Oct21_UsingPropertiesFileAndActionsClass
    Oct21_UsingPropertiesFileAndActionsClass

--- BaseClass.java
+++ BaseClass.java
@@ -1,6 +1,9 @@
 package com.selenium.practice;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,33 +17,46 @@ public class BaseClass {
 	public static void launchActiTimeApplication()
 	{		
 		launchBrowser();
-		driver.get("https://demo.actitime.com/login.do");
-		
-		
+		driver.get(getConfigData("url"));		
 
 	}
 	
 	public static void launchBrowser()
 	{
-		String browser = "firefox";
+		String browser = getConfigData("browser");
 		
-		if(browser.equalsIgnoreCase("chrome"))
+		switch(browser)
 		{
-			System.setProperty("webdriver.chrome.driver", "./utilities/chromedriver.exe");		
-			driver = new ChromeDriver();
-		}
-		else if(browser.equalsIgnoreCase("firefox"))
-		{		
-			System.setProperty("webdriver.gecko.driver", "./utilities/geckodriver.exe");
-			driver = new FirefoxDriver();
+			case "firefox":
+			{
+				System.out.println(" Running the test cases using Firefox browser.....");
+				System.setProperty("webdriver.gecko.driver", "./utilities/geckodriver.exe");
+				driver = new FirefoxDriver();
+				break;
+			}
 		
-		}
+			case "chrome":
+			{	
+				System.out.println(" Running the test cases using chrome browser.....");
+				System.setProperty("webdriver.chrome.driver", "./utilities/chromedriver.exe");		
+				driver = new ChromeDriver();
+				break;
+			}
+			default :
+			{
+				System.out.println(" Browser not specified , using chrome as the default browser..");
+				System.setProperty("webdriver.chrome.driver", "./utilities/chromedriver.exe");		
+				driver = new ChromeDriver();
+				break;
+			}
+		}	
 		
 		driver.manage().window().maximize();
 		// Applying implicit wait . it is applied only once where the driver is created and applicable through out the life cycle of the driver..
 		// polling interval is 500 MS ( half second )
-		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		String s = getConfigData("timeout"); // fetching the timeout from properties file and converting it to Long
+		Long t = Long.parseLong(s);		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(t));
 	}
 	
 	public static void launchApplication(String url)
@@ -62,6 +78,23 @@ public class BaseClass {
 		driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys(password);
 		driver.findElement(By.xpath("//a[@id='loginButton']")).click();		
 		Thread.sleep(5000);
+	}
+	
+	public static String getConfigData(String key) 
+	{		
+				
+			Properties prop = new Properties();
+				try {				
+					File f = new File("config.properties");	
+					FileInputStream fio = new FileInputStream(f);
+					prop.load(fio);					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}			
+				String val = prop.getProperty(key);					
+				return val;
+				
+				
 	}
 
 
